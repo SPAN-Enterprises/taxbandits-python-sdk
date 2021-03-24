5d5f3f57d498fc30c2b3331ee946e44696f2952d
stssubbu <git+subbu@spanenterprises.com>
2021-03-24 17:04:16 +0530

    create business

--- core/CreateBusinessRequest.py
+++ core/CreateBusinessRequest.py
@@ -2,22 +2,24 @@ from core import SigningAuthority
 from core import ForeignAddress
 from core import USAddress
 
-Create = {
-    "BusinessNm": "Subbu LLC",
-    "TradeNm": "Kodak",
-    "IsEIN": True,
-    "EINorSSN": "733313330",
-    "Email": "subbuleaf+1@gmail.com",
-    "ContactNm": "John",
-    "Phone": "1234567890",
-    "PhoneExtn": "12345",
-    "Fax": "1234567890",
-    "BusinessType": "ESTE",
-    "SigningAuthority": SigningAuthority.signingAuthority,
-    "KindOfEmployer": "FEDERALGOVT",
-    "KindOfPayer": "REGULAR941",
-    "IsBusinessTerminated": False,
-    "IsForeign": True,
-    "USAddress": USAddress.usAddress,
-    "ForeignAddress": ForeignAddress.foreignAddress
-}
+
+def create(businessName, einOrSSN):
+    return {
+        "BusinessNm": businessName,
+        "TradeNm": "Kodak",
+        "IsEIN": True,
+        "EINorSSN": einOrSSN,
+        "Email": "subbuleaf+1@gmail.com",
+        "ContactNm": "John",
+        "Phone": "1234567890",
+        "PhoneExtn": "12345",
+        "Fax": "1234567890",
+        "BusinessType": "ESTE",
+        "SigningAuthority": SigningAuthority.signingAuthority,
+        "KindOfEmployer": "FEDERALGOVT",
+        "KindOfPayer": "REGULAR941",
+        "IsBusinessTerminated": False,
+        "IsForeign": True,
+        "USAddress": USAddress.usAddress,
+        "ForeignAddress": ForeignAddress.foreignAddress
+    }
