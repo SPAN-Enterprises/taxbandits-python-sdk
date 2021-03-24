42a044794543341b72c9f4552a4887a8fc92b3ca
stssubbu <git+subbu@spanenterprises.com>
2021-03-24 20:28:20 +0530

    create business

--- utils/Config.py
+++ utils/Config.py
@@ -1,14 +1,14 @@
 userCredential = {
-    "CLIENT_ID": "b01cea6dc87ca261",
-    "SECRET_ID": "7HUO9F3KhfEU92KqtpgqQ",
-    "USER_TOKEN": "98c0bea37cea4af1a73bb0179fd9a806"
+    "CLIENT_ID": "Your Client Id",
+    "SECRET_ID": "Your Secret Id",
+    "USER_TOKEN": "User Token"
 }
 
 apiBaseUrls = {
 
-    "O_AUTH_BASE_URL": "http://oauth.taxforall.com/v2/tbsauth",
+    "O_AUTH_BASE_URL": "Auth URL",
 
-    "TBS_API_BASE_URL": "http://tbsapi.taxforall.com/v1.6.0/"
+    "TBS_API_BASE_URL": "TBS URL"
 }
 
 access_token=''
