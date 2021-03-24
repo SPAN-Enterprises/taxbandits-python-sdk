60b575031bd50eea74f667c0e04e1e70a16fe570
stssubbu <git+subbu@spanenterprises.com>
2021-03-24 16:37:16 +0530

    create business

--- business.py
+++ business.py
@@ -1,23 +1,30 @@
+
 from flask import Flask, render_template, request
 
-business= Flask(__name__)
+business = Flask(__name__)
+
 
 @business.route('/')
 def index():
     return render_template('index.html')
 
-@business.route('/submit', methods=['POST'])
+
+@business.route('/success', methods=['POST'])
 def submit():
     if request.method=='POST':
-        BusinessName=request.form['business_name']
-        EINOrSSN=request.form['einorssn']
-        print(BusinessName,EINOrSSN)
+        BusinessName =request.form['business_name']
+        EINOrSSN =request.form['einorssn']
+        print(BusinessName ,EINOrSSN)
+
         if BusinessName == '' or EINOrSSN == '':
             return render_template('index.html', message='Please enter required fields')
+
+        elif len(EINOrSSN) <=8:
+            return render_template('index.html', message='Please enter valid inut')
+
         return render_template('success.html')
 
 
-
 if __name__ == '__main__':
- business.debug=True
+ business.debug =True
  business.run()
