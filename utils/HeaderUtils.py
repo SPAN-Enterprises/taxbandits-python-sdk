3427969f0d5d6aeaef6f0a310a98e6596aee6bfa
stssubbu <git+subbu@spanenterprises.com>
2021-04-08 15:34:54 +0530

    jwt utils

--- utils/HeaderUtils.py
+++ utils/HeaderUtils.py
@@ -1,10 +1,9 @@
 from api_services import JwtGeneration
 from utils import Config
 
+
 def getheaders():
-
     if Config.access_token is None or len(Config.access_token) == 0:
-
         jwtToken = JwtGeneration.get_jwt_token()
 
         JwtGeneration.get_access_token_by_jwt_token(jwtToken)
