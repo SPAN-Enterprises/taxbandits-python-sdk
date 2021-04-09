65d770df641770535c41eb1f58421d9135fa9564
stssubbu <git+subbu@spanenterprises.com>
2021-04-09 18:57:36 +0530

    ngrok

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app = Flask(__name__)
 port = 5000
 
 # Open a ngrok tunnel to the HTTP server
-public_url = ngrok.connect(port).public_url
-print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+public_url = ngrok.connect(port).public_url+"/getWebhook"
+print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/getWebhook\"".format(public_url, port))
 
 # Update any base URLs to use the public ngrok URL
 app.config["BASE_URL"] = public_url
@@ -21,14 +21,14 @@ app.config["BASE_URL"] = public_url
 
 
 # Define Flask routes
-@app.route("/")
+@app.route("/getWebhook")
 def index():
     json_content = request.json
     print (json.dumps(json_content))
     return "EMPTY"
 
 
-@app.route("/success" , methods=['POST'])
+@app.route("/getWebhook" , methods=['POST'])
 def index1():
     json_content = request.json
     print ("index1"+json.dumps(json_content))
@@ -37,10 +37,10 @@ def index1():
                                     "Business", ErrorMessage=' Business Created Successfully')
 
 
-@app.route("/" , methods=['GET'])
+@app.route("/getWebhook" , methods=['GET'])
 def index2():
     json_content = request.json
-    print ("index1"+json.dumps(json_content))
+    print ("index2"+json.dumps(json_content))
     return "POST"
 
 # Start the Flask server in a new thread
