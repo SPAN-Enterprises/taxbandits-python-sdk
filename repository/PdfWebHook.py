b7ae3978ce92eaf2b30f0a0c710bcdd05c1b01e8
stssubbu <git+subbu@spanenterprises.com>
2021-04-13 13:05:13 +0530

    Get PDF Completed

--- repository/PdfWebHook.py
+++ repository/PdfWebHook.py
@@ -5,9 +5,9 @@ import pymongo
 
 def save_response_in_mongodb(response):
     client = pymongo.MongoClient(
-        "mongodb+srv://subbuleaf:d$$9943111606@cluster0.kaf9y.mongodb.net/pythonSDK?retryWrites=true&w=majority&authSource=admin",
+        "mongodb+srv://<username>:<password>@cluster0.kaf9y.mongodb.net/<database_name>?retryWrites=true&w=majority&authSource=admin",
         ssl_cert_reqs=ssl.CERT_NONE)
-    my_database = client["pythonSDK"]
-    my_collection = my_database["FormNEC"]
+    my_database = client["<database_name>"]
+    my_collection = my_database["<collection_name>"]
     entity = json.loads(response)
     return my_collection.save(entity)
