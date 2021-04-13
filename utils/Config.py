ef0960330933b1981e8e4079a81b650137f7b0a4
stssubbu <git+subbu@spanenterprises.com>
2021-04-13 12:45:01 +0530

    Get PDF Completed

--- utils/Config.py
+++ utils/Config.py
@@ -1,7 +1,7 @@
 userCredential = {
-    "CLIENT_ID": "9757c9c24be51be5",
-    "SECRET_ID": "u6v1CPFsL1GuNNz4ipnRw",
-    "USER_TOKEN": "3519b7e697384a0391d93485e07d60ee"
+    "CLIENT_ID": "Your Client ID",
+    "SECRET_ID": "Your Secret ID",
+    "USER_TOKEN": "Your User Token"
 
 }
 
